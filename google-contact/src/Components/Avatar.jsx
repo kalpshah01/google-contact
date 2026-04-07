a757a76b9d97126ae400bdbca0d2ba76db7ae34b
Kalp shah <kalpshah962005@gmail.com>
2026-04-08 02:09:27 +0530

    Delete google-contact directory

--- google-contact/src/Components/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-import { colorFor, initials } from "../utils/contactUtils";
-
- const Avatar = ({ contact, size = 40 }) => (
-      <div className="avatar" style={{ width: size, height: size, background: colorFor(contact.first), borderRadius: '50%', display:'flex', alignItems:'center', justifyContent:'center', fontFamily:"'Google Sans',sans-serif", fontWeight:500, color:'white', fontSize: size * .4, flexShrink:0 }}>
-        {initials(contact)}
-      </div>
-    );
-    export default Avatar;
